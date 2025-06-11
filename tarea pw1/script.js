903bfcde9e300cc2a7d962fc3148cfa96894255f
Magali <magalipeschiera@gmail.com>
2025-06-11 20:05:20 -0300

    Add files via upload

---
+++ tarea pw1/script.js
@@ -0,0 +1,63 @@
+// FORMULARIO
+document.getElementById("contact-form").addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const nombre = document.getElementById("nombre").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const fecha = document.getElementById("fecha").value.trim();
+  const mensaje = document.getElementById("mensaje");
+
+  if (nombre === "" || email === "" || fecha === "") {
+    mensaje.style.color = "red";
+    mensaje.textContent = "Todos los campos son obligatorios.";
+    return;
+  }
+
+  if (!email.includes("@") || !email.includes(".")) {
+    mensaje.style.color = "red";
+    mensaje.textContent = "Por favor, ingresá un correo válido.";
+    return;
+  }
+
+  try {
+    const response = await fetch("https://formspree.io/f/xdkzeqkd", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        nombre: nombre,
+        email: email,
+        fecha: fecha
+      })
+    });
+
+    if (response.ok) {
+      mensaje.style.color = "green";
+      mensaje.textContent = "¡Reserva enviada con éxito!";
+      e.target.reset();
+    } else {
+      mensaje.style.color = "red";
+      mensaje.textContent = "Ocurrió un error al enviar la reserva.";
+    }
+  } catch (error) {
+    mensaje.style.color = "red";
+    mensaje.textContent = "No se pudo conectar con el servidor.";
+  }
+});
+
+// MODO OSCURO / CLARO
+const botonModo = document.getElementById('modo-btn');
+const body = document.body;
+const secciones = document.querySelectorAll('.barra-superior, .seccion, footer, form');
+
+botonModo.addEventListener('click', () => {
+  body.classList.toggle('modo-oscuro');
+  secciones.forEach(sec => sec.classList.toggle('modo-oscuro'));
+
+  if (body.classList.contains('modo-oscuro')) {
+    botonModo.textContent = '☀️ Modo Claro';
+  } else {
+    botonModo.textContent = '🌙 Modo Oscuro';
+  }
+});
